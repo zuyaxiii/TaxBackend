dcf9b4ff7929866c059395dd2ce4aea27611730a
zuyaxiii <akiratta1810@gmail.com>
2025-03-03 16:48:58 +0700

    Update Domain

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: 'http://localhost:3000', 
+    origin: ['https://tax-condo.vercel.app', 'http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });
